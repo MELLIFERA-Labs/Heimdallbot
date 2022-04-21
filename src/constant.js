b2be60d83e15aad23430f8e013f06ec6b8ac8d4f
ruslan glaznyov <ruslan.glaznyov@gmail.com>
2022-04-21 13:09:04 +0300

    feature: move url proposal to global.config.js

--- src/constant.js
+++ src/constant.js
@@ -4,5 +4,4 @@ module.exports = {
     TELEGRAM_CONFIG_PATH: './config/telegram.config.js',
     DISCORD_CONFIG_PATH: './config/discord.config.js',
     GLOBAL_CONFIG_PATH: './config/global.config.js',
-    TO_VOTE_PROPOSAL_URL: 'https://mainnet.odinprotocol.io/proposal/',
 }
